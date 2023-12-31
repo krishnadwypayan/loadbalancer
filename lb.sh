94a9527d183f9a5ec4067076436309dcdce15ff0
Krishna Kota <kota_k@apple.com>
2023-12-31 15:41:24 +0530

    more changes to dockerfiles

--- lb.sh
+++ lb.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run -p 80:8080 -p 9000:9000 krishnadwypayan/lb
+docker run -p 80:8080 -p 9000:9000 krishnadwypayan/lb-service
+docker run -p 8090:8090 -p 9001:9001 krishnadwypayan/backend-service
